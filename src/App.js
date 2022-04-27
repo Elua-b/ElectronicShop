5216334d8e73e177d72ffb2039dabb5c153375e9
Elua-b <bugingoeloi@gmail.com>
2022-04-27 10:36:55 +0200

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
-// import Home from "./home";
-import "./App.css";
-import React ,{useState} from "react";
-// import About from "./about";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function App(){
-  const[count,setCount]=useState(4)
-  const [theme, setTheme]=useState('number');
-  function decreamentCount(){
-    setCount(count-1)
-    setTheme();
-  }
-  function increamentCount(){
-    setCount(count+1)
-    setCount(count+1)
-    setTheme("number")
-  }
-  return(
+import React, { useState } from "react";
+import data from "./components/back/Data/data";
+import Header from "./components/front/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyRoutes from "./components/front/Header/Routes/Routes";
+import Search from "./components/Search";
+import Products from "./components/front/Header/products/Products";
+import Product from "./components/front/Header/products/Product";
+import Signup from "./components/front/signup/Signup";
+import Cart from "./components/front/cart/Cart";
+const App = () => {
+  const { productItems } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const handleAddProduct = (product) => {
+    const ProductExist = cartItems.find((item) => item.id === product.id);
+    if (ProductExist) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...ProductExist, quantity: ProductExist.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+  return (
     <>
-    <button   onClick={decreamentCount}>-</button>
-    <span>{count}</span>
-    <span>{theme}</span>
-    <button onClick={increamentCount}>+</button>
-    
+      <BrowserRouter>
+        <Header />
+        <Search />
+        <Routes>
+          {data.productItems.map((product) => (
+            <Route
+              path={`cart/${product.name}`}
+              element={<Product product={product} />}
+            />
+          ))}
+          <Route
+            path="/"
+            element={
+              <Products
+                productItems={productItems}
+                handleAddProduct={handleAddProduct}
+              />
+            }
+            exact
+          />
+          <Route path="/signup" element={<Signup />} exact />
+          <Route
+            path="/cart"
+            element={
+              <Cart cartItems={cartItems} handleAddProduct={handleAddProduct} />
+            }
+            exact
+          />
+        </Routes>
+        <MyRoutes
+          productItems={productItems}
+          cartItems={cartItems}
+          handleAddProduct={handleAddProduct}
+        />
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/back/Data/data.js
@@ -0,0 +1,72 @@
+
+// import React from 'react';
+
+// const products=()=>{
+// <div className="prod">
+    
+// </div>
+
+// }
+
+
+
+
+const data ={
+    productItems:[
+        {
+            id:"1",
+            name:"camera",
+            price:895,
+            Image:"pics/camera.jpg"
+        },
+        {
+            id:"2",
+            name:"Hcomputer",
+            price:860,
+            Image:"/pics/comp.jpg"
+        },
+        {
+            id:"3",
+            name:"hardcomponents",
+            price:400,
+            Image:"/pics/hardcomponents.jpg"
+        },
+        {
+            id:"4",
+            name:"headsets",
+            price:700,
+            Image:"/pics/headset.jpg"
+        },
+        {
+            id:"5",
+            name:"phone",
+            price:1095,
+            Image:"/pics/phone.jpg"
+        },
+        {
+            id:"6",
+            name:"playStation",
+            price:995,
+            Image:"/pics/ps.jpg"
+        },
+        {
+            id:"7",
+            name:"smart phones",
+            price:1995,
+            Image:"/pics/smartphones.jpg"
+        },
+        {
+            id:"8",
+            name:"computer",
+            price:1895,
+            Image:"/pics/comp.jpg"
+        },
+         {
+           id:"9",
+             name:"Remotes",
+             price:2895,
+             Image:"/pics/remote.jpg"
+         }
+    ]
+}
+export default data
